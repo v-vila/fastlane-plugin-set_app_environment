b88756e09d8adfe9e1dd67f8c49ae83122c53147
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-22 13:28:19 +0100

    fix: test

--- spec/set_app_environment_action_spec.rb
+++ spec/set_app_environment_action_spec.rb
@@ -7,14 +7,14 @@ describe Fastlane::Actions::SetAppEnvironmentAction do
     end
 
     it 'executes shell command to set app environment' do
-      allow(Fastlane::Actions).to receive(:sh)
+      allow(Fastlane).to receive(:sh)
 
       params = {
         env: "test_env",
         env_file_path: "path/to/env_file"
       }
 
-      expect(Fastlane::Actions).to receive(:sh).with("jq '{ \"test_env\": .test_env }' path/to/env_file > environments.tmp.json && mv environments.tmp.json path/to/env_file")
+      expect(Fastlane).to receive(:sh).with("jq '{ \"test_env\": .test_env }' path/to/env_file > environments.tmp.json && mv environments.tmp.json path/to/env_file")
 
       Fastlane::Actions::SetAppEnvironmentAction.run(params)
     end
